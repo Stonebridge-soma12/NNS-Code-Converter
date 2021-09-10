058abad84d2130a6851490f800923b3722b812a8
elixter <ltw971@naver.com>
2021-09-10 20:15:50 +0900

    chore: clean up code

--- main.go
+++ main.go
@@ -20,36 +20,7 @@ func main() {
 	e.GET("/model", GetSavedModel)
 
 	e.POST("/make-python", MakeModel)
-	e.POST("/publish/epoch/end", TrainMonitor)
 	e.POST("/fit", Fit)
-
-	//e.POST("/api/trainedModel", func(c echo.Context) error {
-	//	form, err := c.MultipartForm()
-	//	if err != nil {
-	//		fmt.Println(err)
-	//		return err
-	//	}
-	//	files := form.File["files"]
-	//
-	//	for _, file := range files {
-	//		src, err := file.Open()
-	//		if err != nil {
-	//			return err
-	//		}
-	//		defer src.Close()
-	//
-	//		dst, err := os.Create(file.Filename)
-	//		if err != nil {
-	//			return err
-	//		}
-	//		defer dst.Close()
-	//		if _, err = io.Copy(dst, src); err != nil {
-	//			return err
-	//		}
-	//	}
-	//
-	//	return nil
-	//})
 
 	e.Logger.Fatal(e.Start(":8081"))
 }
